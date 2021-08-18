4830b3116fa4e00fc2458455c04817c392e60c31
callingpizza <68723482+callingpizza@users.noreply.github.com>
2021-08-18 21:41:09 +0530

    Added a header and footer containing discord server link, cleaned code, update css

--- js/index.js
+++ js/index.js
@@ -10,7 +10,9 @@ var firebaseConfig = {
 	appId: "1:843641053169:web:c4a822fb20e55c68d12e50",
 	measurementId: "G-FC4567M3EP"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-m.render(document.getElementsByTagName("main")[0], m("h1", {style: "text-align: center;"}, "Hello world!"));
+
+m.render(document.getElementsByTagName("main")[0], m("h1", {style: "text-align: center;"}, ""));
