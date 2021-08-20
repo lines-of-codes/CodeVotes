dca4f64ae27123188bfffdc901fa3944b49d7e9e
Satakun Utama <49134472+lines-of-codes@users.noreply.github.com>
2021-08-21 04:38:15 +0700

    Added Dark theme

--- js/themeManager.js
+++ js/themeManager.js
@@ -20,9 +20,10 @@ function setColorTheme(theme) {
 		document.body.style.setProperty("--font-color", "black");
 	} else if(theme === "dark") {
 		localStorage.setItem("theme", "dark");
-		document.body.style.setProperty("--background", "white");
-		document.body.style.setProperty("--darker-background", "whitesmoke");
-		document.body.style.setProperty("--darker-background-hover", "lightgray");
+		document.body.style.setProperty("--background", "#23272a");
+		document.body.style.setProperty("--darker-background", "#2c2f33");
+		document.body.style.setProperty("--darker-background-hover", "gray");
+		document.body.style.setProperty("--font-color", "white");
 	} else if(theme === "solarized") {
 		localStorage.setItem("theme", "solarized");
 		document.body.style.setProperty("--background", "#002b36");
