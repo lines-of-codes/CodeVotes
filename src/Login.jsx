58cbfe4090ca4c2152ea95e105dd192c031c232f
Satakun Utama <49134472+lines-of-codes@users.noreply.github.com>
2021-10-30 17:44:08 +0700

    Edit Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,23 @@
+import { useRef } from 'react';
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 import { auth } from './components/firebase';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+
     function handleSubmit(event) {
         event.preventDefault();
+
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredentials) => {
+                window.location.href = "/";
+            }).catch((error) => {
+                console.error(error);
+                alert(`An error occured during signing you in!\n${error.message}`);
+            });
     }
 
     return (
@@ -14,12 +26,14 @@ export default function Login() {
             <main className="centered-column-flexbox">
                 <h1>Login</h1>
                 <form onSubmit={handleSubmit} className="centered-column-flexbox">
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" /><br/>
+                    <input ref={email} type="email" placeholder="Email" />
+                    <input ref={password} type="password" placeholder="Password" /><br/>
                     <input type="submit" className="btn1" value="Login" />
                     <span style={{lineHeight: "40px"}}>- Or -</span>
                     <button className="loginservicebtn" id="btngoogle"></button>
-                    <button className="loginservicebtn" id="btngithub">Sign in with GitHub</button>
+                    <button className="loginservicebtn" id="btngithub">
+                        Sign in with GitHub
+                    </button>
                 </form>
             </main><br/>
             <Footer />
